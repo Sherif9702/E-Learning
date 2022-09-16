9284556033c2efe65f43ef61a8ea9b063e997110
cherif_hanne <cherif-athana.hanne@universite-paris-saclay.fr>
2022-09-16 10:51:44 +0200

    first test in IJ with Java

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Point point1 = new Point(1,2);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println("Test1");
+        System.out.println(point1);
+        System.out.println(point2);
+
+    }
+}
